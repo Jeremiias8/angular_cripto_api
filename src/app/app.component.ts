ab25c3bfe0dce6b995bdd4c448ed7db918d80e28
Jeremías <jeremiias122002@gmail.com>
2023-12-06 17:21:43 +0100

    first commit of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_repaso';
+  // title = 'angular_repaso';
+  coins: Coin[] = [];
+  filteredCoins: Coin[] = [];
+  titles: string [] = [
+    '#',
+    'Coin',
+    'Price',
+    'Price Change',
+    '24h Volume'
+  ];
+
+  searchText = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  searchCoin() {
+
+    this.filteredCoins = this.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+
+  ngOnInit(): void {
+
+    console.log("A punto de llamar a la url de CRIPTO APIs.");
+
+    this.http.get<Coin[]>("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+
+          this.coins = res;
+          this.filteredCoins = res;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+  }
 }
